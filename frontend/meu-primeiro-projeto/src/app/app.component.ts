6abc65d9b21ecdefcf2b1ace3c09809bb7b7f066
Marcos <marcosviniciohd@hotmail.com>
2023-06-27 23:20:00 -0300

    9.8 usando variável de referencia

--- frontend/meu-primeiro-projeto/src/app/app.component.ts
+++ frontend/meu-primeiro-projeto/src/app/app.component.ts
@@ -6,16 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nome = 'Marcos Vinício';
+  nome = 'Marcos';
+  sobreNome: string = '';
 
-  adicionar(){
+  adicionar(nome: string, sobreNome: string) {
+    this.nome = nome;
+    this.sobreNome = sobreNome;
+    console.log(`Adicionado nome: ${this.nome} e sobreNome: ${this.sobreNome}`);
+  }
+
+  /* adicionar(){
     console.log(`Adicionando ${this.nome}`);
     const numero: number = Math.round(Math.random() * 100);
     this.nome = 'João ' + numero;
-  }
+  } */
 
-  alterarNome(event: any): void {
+  /* alterarNome(event: any): void {
     //console.log(event);
     this.nome = event.target.value;
-  }
+  } */
 }
