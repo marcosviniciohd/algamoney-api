3d8e1d7e2b62a57bfbfe45d5cfaa17e722af485e
Marcos <marcosviniciohd@hotmail.com>
2023-07-01 19:44:31 -0300

    Utilizando o @Input

--- frontend/meu-primeiro-projeto/src/app/app.module.ts
+++ frontend/meu-primeiro-projeto/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FuncionarioCardComponent,
   ],
   imports: [
     BrowserModule,
